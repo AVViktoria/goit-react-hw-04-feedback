2eda32f6e8a69a8a2b33a97d7a4d1b16562fe24d
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-11-09 20:02:32 -0800

    del all comments

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,4 @@
-// import React, { Component } from 'react';
-
+import { useState } from 'react';
 import '../index.scss'
 
 //*    components    //
@@ -7,7 +6,7 @@ import FeedbackOptions from 'components/FeedbackOptions';
 import Notification from 'components/Notification';
 import Statistics from 'components/Statistics';
 import Section from 'components/Section';
-import { useState } from 'react';
+
 
 // //*      Root      //
 export default function App() {
@@ -36,23 +35,15 @@ const [bad, setBad] = useState(0);
 
   // //*  total Fn   //
   const  countTotalFeedback = () => {
-  //   const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
   // //*  percentage Fn   //
   const  countPositiveFeedbackPercentage = () => {
-  //   const { good } = this.state;
     return countTotalFeedback()
       ? Math.round((good / countTotalFeedback()) * 100)
       : 0;
   };
-
-  // render() {
-  //   const { good, neutral, bad } = this.state;
-  //   const options = Object.keys(this.state);
-
-
 
     return (
       <div className = "container" >
@@ -78,67 +69,3 @@ const [bad, setBad] = useState(0);
       </div>
     );
   }
-// }
-
-
-
-
-
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   //*  handle fn for buttons   //
-//   handleLeaveFeedback = option => {
-//     this.setState(prevState => ({
-//       [option]: prevState[option] + 1,
-//     }));
-//   };
-
-//   //*  total Fn   //
-//   countTotalFeedback = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good + neutral + bad;
-//   };
-
-//   //*  percentage Fn   //
-//   countPositiveFeedbackPercentage = () => {
-//     const { good } = this.state;
-//     return this.countTotalFeedback()
-//       ? Math.round((good / this.countTotalFeedback()) * 100)
-//       : 0;
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const options = Object.keys(this.state);
-//     return (
-//       <div className = "container" >
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={options}
-//             onLeaveFeedback={this.handleLeaveFeedback}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {!this.countTotalFeedback() ? (
-//             <Notification message="There is no feedback" />
-//           ) : (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positiveFeedback={this.countPositiveFeedbackPercentage()}
-//             />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
